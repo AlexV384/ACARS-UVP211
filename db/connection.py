67a6516442d6ffb3b99b8b9de5ac4de5d41dcee9
robos <robostep9829@gmail.com>
2026-07-14 14:27:42 +0300

    Повышение connection.py до синглтона 👑

--- db/connection.py
+++ db/connection.py
@@ -1,6 +1,9 @@
 import asyncpg
 from config import DB_CONFIG
-
+_pool: asyncpg.Pool | None = None
 
 async def get_pool() -> asyncpg.Pool:
-    return await asyncpg.create_pool(**DB_CONFIG, min_size=1, max_size=5)
+    global _pool
+    if _pool is None:
+        _pool = await asyncpg.create_pool(**DB_CONFIG, min_size=1, max_size=5)
+    return _pool
